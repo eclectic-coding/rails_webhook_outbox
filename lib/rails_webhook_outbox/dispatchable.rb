983ae6923b7d8b9d504cae40949abb1be8f67f97
Chuck Smith <eclectic-coding@users.noreply.github.com>
2026-06-26 10:01:58 -0400

    Dispatchable feature

---
+++ lib/rails_webhook_outbox/dispatchable.rb
@@ -0,0 +1,42 @@
+module RailsWebhookOutbox
+  module Dispatchable
+    extend ActiveSupport::Concern
+
+    included do
+      class_attribute :_webhook_dispatches, instance_writer: false
+      self._webhook_dispatches = []
+    end
+
+    class_methods do
+      def dispatches_webhook(event, on:, **options)
+        condition = options[:if]
+        self._webhook_dispatches = _webhook_dispatches + [{ event: event, on: on, condition: condition }]
+        send(:"after_#{on}", -> { _dispatch_webhook(event, condition) })
+      end
+    end
+
+    def webhook_payload
+      as_json
+    end
+
+    private
+
+    def _dispatch_webhook(event, condition)
+      return if condition && !instance_exec(&condition)
+
+      payload = webhook_payload
+
+      Subscription.active.each do |subscription|
+        next unless subscription.subscribes_to?(event)
+
+        delivery = Delivery.create!(
+          subscription: subscription,
+          event: event,
+          payload: payload
+        )
+
+        DeliveryJob.perform_later(delivery)
+      end
+    end
+  end
+end
